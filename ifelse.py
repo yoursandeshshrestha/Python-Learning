e2f2429dbe775d304bb0cdb8aea24fd0c5a8f1a0
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-15 23:39:29 +0530

    .

--- ifelse.py
+++ ifelse.py
@@ -5,13 +5,27 @@
 # else:
 #     print("You are not eligable to drive")
 
-color = "pink"
+# color = "pink"
 
-if(color == "green"):
-    print("You can Go")
-elif(color == "Red"):
-    print("Stop")
-elif(color == "Yellow"):
-    print("Drive Slow")
+# if(color == "green"):
+#     print("You can Go")
+# elif(color == "Red"):
+#     print("Stop")
+# elif(color == "Yellow"):
+#     print("Drive Slow")
+# else:
+#     print("Light is Broken")
+
+
+marks = int(input("Enter your marks:"))
+
+if(marks >= 90):
+    print("A")
+elif(marks >= 80):
+    print("B")
+elif(marks >= 70):
+    print("C")
+elif(marks < 70):
+    print("D")
 else:
-    print("Light is Broken")
+    print("Fail")
