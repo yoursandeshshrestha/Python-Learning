e87097cd5f45a400b2f9b4111edba29e59d09968
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-15 23:40:17 +0530

    test 3

---
+++ practice.py
@@ -0,0 +1,25 @@
+# number = int(input("Enter the number:"))
+# if(number % 2 == 0):
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+# number1 = int(input("Enter a number :"))
+# number2 = int(input("Enter a number :"))
+# number3 = int(input("Enter a number :"))
+
+# if(number1>=number2 and number1>=number3):
+#     print("The greatest number is number1", number1)
+# elif(number2>=number1 and number2>=number3):
+#     print("The greatest number is number2", number2)
+# elif(number3>=number1 and number3>=number2):
+#     print("The greatest number is number3", number3)
+
+
+number = int(input("Enter the number : "))
+
+if(number % 7 == 0):
+    print("{0} is multiple of 7".format(number))
+else:
+    print("{0} is not multiple of 7".format(number))
+
