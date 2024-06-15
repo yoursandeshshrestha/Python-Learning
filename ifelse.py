abc253fba132bf384ea7ab8d7d287712ca40154d
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-15 20:28:23 +0530

    first commit

---
+++ ifelse.py
@@ -0,0 +1,6 @@
+age = int(input("What's your age :"))
+
+if (age >= 18):
+    print("You are eligable to drive")
+else:
+    print("You are not eligable to drive")
---
+++ python.py
@@ -0,0 +1,31 @@
+# Number1 = int(input("Enter First Number:"))
+# Number2 = int(input("Enter Second Number:"))
+# print("Your First Number was:", Number1)
+# print("Your Second Number was:", Number2)
+# print("Total:", Number1+Number2)
+
+# side = float(input("Enter side:"))
+# print("Area of side:", side*side)
+
+# Num1 = float(input("Enter first number:"))
+# Num2 = float(input("Enter Second number:"))
+# print("The avarage is :", (Num1+Num2)/2)
+
+# a = int(input("First Number:"))
+# b = int(input("Second Number:"))
+# print(a>=b)
+
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# sum = num1 + num2
+# print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+# name = "sandesh"
+# print(len(name))
+
+# username = input("What's your name:")
+# print(len(username))
+
+string = "san$desh$"
+print(string.count("$"))
+
