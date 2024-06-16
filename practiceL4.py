248d5347d8ef6e2fee0d0271f7c07b1ef0f139a6
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-16 12:48:23 +0530

    question

---
+++ practiceL4.py
@@ -0,0 +1,12 @@
+marks = {}
+
+mark = input("Enter python marks : ")
+marks.update({"Python": mark} )
+
+mark = input("Enter Java marks : ")
+marks.update({"Java": mark} )
+
+mark = input("Enter JavaScript marks : ")
+marks.update({"JavaScript": mark} )
+
+print(marks)
