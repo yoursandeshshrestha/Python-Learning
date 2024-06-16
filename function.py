d9103e031eac43f468c8bb827b8aca8e1ad8748f
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-16 16:08:09 +0530

    function

---
+++ function.py
@@ -0,0 +1,18 @@
+# def Q1(something):
+#     print(len(something))
+#     return something
+
+# Q1([1, 2, 3, 4])
+
+# list = ["batman", "superman", "spiderman"]
+
+# def something(list):
+#     print(list)
+#     return(list)
+
+# something(list)
+
+
+
+
+
