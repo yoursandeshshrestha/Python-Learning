aff4d7490d47b5df6231bf69ade3eb29f4bce6eb
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-16 16:08:19 +0530

    while

---
+++ while.py
@@ -0,0 +1,29 @@
+# num = 1
+
+# while num <= 100:
+#     print(num)
+#     num = num +1
+
+# i = 100
+
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# i = 1
+# while i <= 10:
+#     print(10*i)
+#     i+=1
+
+
+numbers = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# ind= 0
+# while ind < len(list):
+#     print(list[ind])
+#     ind+=1
+
+for number in numbers:
+    if(number == 81):
+        continue
+    print(number)
