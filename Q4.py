31fc99d3d47ce4b1d018ac39c83532cdf70ee694
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-17 00:18:58 +0530

    .

---
+++ Q4.py
@@ -0,0 +1,10 @@
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+largest = max(num1, num2, num3)
+smallest = min(num1, num2, num3)
+
+print(f"The larget is {largest}")
+print(f"The smallest is {smallest}")
