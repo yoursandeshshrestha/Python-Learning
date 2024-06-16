62f5b9d49edc01794da7aead2059c9e92a68284c
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-17 00:18:45 +0530

    .

---
+++ Q2.py
@@ -0,0 +1,17 @@
+
+def display_data_types():
+    int_var = 10
+    print(f'The type of {int_var} is {type(int_var)}')
+
+    float_var = 10.5
+    print(f'The type of {float_var} is {type(float_var)}')
+
+    string_var = "Hello, World!"
+    print(f'The type of "{string_var}" is {type(string_var)}')
+
+    list_var = [1, 2, 3, 4, 5]
+    print(f'The type of {list_var} is {type(list_var)}')
+
+display_data_types()
+
+
