5d061f75bf4d4dda41fd42f667c7067c0fd48019
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-17 00:18:39 +0530

    .

---
+++ Q1.py
@@ -0,0 +1,10 @@
+def display_information():
+    name = "Sandesh Shrestha"
+    college_name = "Inspiria Knowledge Campus"
+    message = "Welcome to the program!"
+
+    print("Name:", name)
+    print("College Name:", college_name)
+    print("Message:", message)
+
+display_information()
