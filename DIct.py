5d729e9d0ce9a7beff4916a7abd70b7441b4205d
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-16 12:48:08 +0530

    dict

---
+++ DIct.py
@@ -0,0 +1,12 @@
+Dict = {
+    "table": ("a piece of furniture", "list of facts & figures"),
+    "cat": "a small animal"
+}
+
+print(Dict)
+
+newSet = {"python", "java", "C++", "python", "javascript", "java"
+          "python", "java", "C++", "c"}
+
+print(newSet)
+print(len(newSet))
