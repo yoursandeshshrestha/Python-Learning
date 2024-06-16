6783498d1740441c6eb812b83d10f96e1fc04a45
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-17 00:18:53 +0530

    .

---
+++ Q3.py
@@ -0,0 +1,15 @@
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print(f"The larger number is {num1}")
+    print(f"The smaller number is {num2}")
+elif num2 > num1:
+    print(f"The larger number is {num2}")
+    print(f"The smaller number is {num1}")
+else:
+    print("Both numbers are equal")
+
+
+    
