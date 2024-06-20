69acd218ab24dfc31e2b923eaf0227d127972ee6
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-06-20 21:25:12 +0530

    class

---
+++ class.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+person1= Person("sandesh shrestha", 19)
+
+person1.display()                       
